d8dc32f5f0fef55fe748e6666036e25f627988ab
NiBo <nibocn@gmail.com>
2018-12-16 15:31:38 +0800

    feat(brew): Add brew package install script

---
+++ brew.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+brew update
+
+brew upgrade
+
+brew install coreutils
+
+# Python
+brew install python@2
+brew install python@3
+brew install pyenv
+brew install pipenv
+
+# zsh plugin
+brew install zsh-autosuggestions
+brew install zsh-syntax-highlighting
+
+brew install thefuck
+
+brew install git
+
+brew install tmux
+
