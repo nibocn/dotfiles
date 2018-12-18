598e5251fccd841d136b53b86ebb93734069889d
NiBo <nibocn@gmail.com>
2018-12-18 10:59:26 +0800

    fix(bash): 重复创建目录软链接错误

--- softlink.sh
+++ softlink.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+ln -snf ~/.local/dotfiles/dircolors ~/.dircolors
+
 ln -sf ~/.local/dotfiles/.gitconfig ~/.gitconfig
 
 mkdir -p ~/.pip
@@ -8,4 +10,4 @@ ln -sf ~/.local/dotfiles/.pip/pip.conf ~/.pip/pip.conf
 ln -sf ~/.local/dotfiles/.tmux.conf ~/.tmux.conf
 
 mkdir -p ~/.vim
-ln -sf ~/.local/dotfiles/vim/UltiSnips ~/.vim/UltiSnips
+ln -snf ~/.local/dotfiles/vim/UltiSnips ~/.vim/UltiSnips
