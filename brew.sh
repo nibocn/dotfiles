028fe6e7be4ba7b672cf2f84494c89b04ca12a6b
NiBo <nibocn@gmail.com>
2019-03-22 09:24:15 +0800

    refactor(brew): 调整 brew 包安装脚本

--- brew.sh
+++ brew.sh
@@ -2,47 +2,67 @@
 
 # Install command-line tools using Homebrew.
 
+ask() {
+  while true; do
+    read -p "$1 ([y]/n) " -r
+    REPLY=${REPLY:-"y"}
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      return 1
+    elif [[ $REPLY =~ ^[Nn]$ ]]; then
+      return 0
+    fi
+  done
+}
+
+
 brew update
 
-brew upgrade
-brew cask upgrade
-
-brew install coreutils
-
-brew install zsh
-
-# Python
-brew install python@2
-brew install python@3
-brew install pyenv
-brew install pipenv
-
-brew install thefuck
-brew install git
-brew install tmux
-brew install macvim
-brew install tldr
-brew install autojump
-brew install you-get
-brew install the_silver_searcher
-brew install exa
-
-# fzf
-brew install fzf
-$(brew --prefix)/opt/fzf/install
-brew install highlight
+ask "是否要升级已存在的软件包？"
+if [ $? -eq 1 ]; then
+  brew upgrade
+  brew cask upgrade
+fi
 
 
-brew cask install postman
-brew cask install dbeaver-community
-brew cask install mpv
-brew cask install iina
-brew cask install snipaste
-brew cask install sublime-text
-brew cask install visual-studio-code
-brew cask install flux
-brew cask install xmind-zen
-brew cask install calibre
-brew cask install pomotodo
-# 修改键盘映射
-brew cask install karabiner-elements
+# brew package lists
+brewPackages=("coreutils zsh python@2 python@3 pyenv pipenv thefuck git
+               tmux macvim tldr autojump you-get the_silver_searcher
+               exa highlight")
+
+for package in $brewPackages
+do
+  if brew ls --versions $package > /dev/null; then
+    echo "$package existed"
+  else
+    brew install $package
+  fi
+done
+
+
+if brew ls --versions fzf > /dev/null; then
+  echo "fzf existed"
+else
+  brew install fzf
+  $(brew --prefix)/opt/fzf/install
+fi
+
+
+# brew cask package lists
+brewcaskPackages=("postman dbeaver-community mpv iina snipaste sublime-text
+                   visual-studio-code flux xmind-zen calibre pomotodo
+                   karabiner-elements")
+
+for package in $brewcaskPackages
+do
+  if brew cask ls --versions $package > /dev/null; then
+    echo "$package existed"
+  else
+    brew cask install $package
+  fi
+done
+
+
+ask "是否要清理已下载的软件安装包？"
+if [ $? -eq 1 ]; then
+  brew cleanup
+fi
