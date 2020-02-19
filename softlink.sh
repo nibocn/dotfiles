3f786c93d1090633e9ee88baca0c222ff9d608bd
NiBo <nibocn@gmail.com>
2020-02-19 18:40:37 +0800

    refactor(vim): 根据重构后的 vim 相关配置调整软链接的生成

--- softlink.sh
+++ softlink.sh
@@ -8,12 +8,14 @@ mkdir -p ~/.pip
 ln -sf ~/.local/dotfiles/pip.conf ~/.pip/pip.conf
 
 mkdir -p ~/.vim
-ln -snf ~/.local/dotfiles/UltiSnips ~/.vim/UltiSnips
+ln -snf ~/.local/dotfiles/vim/UltiSnips ~/.vim/UltiSnips
+ln -snf ~/.local/dotfiles/vim/snippets ~/.vim/snippets
 ln -sf ~/.local/dotfiles/vim/coc-settings.json ~/.vim/coc-settings.json
 
 mkdir -p ~/.config/nvim
-ln -snf ~/.local/dotfiles/UltiSnips ~/.config/nvim/UltiSnips
-ln -sf ~/.local/dotfiles/neovim/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -snf ~/.local/dotfiles/vim/UltiSnips ~/.config/nvim/UltiSnips
+ln -snf ~/.local/dotfiles/vim/snippets ~/.config/nvim/snippets
+ln -sf ~/.local/dotfiles/vim/coc-settings.json ~/.config/nvim/coc-settings.json
 
 mkdir -p ~/.config/ranger
 ln -sf ~/.local/dotfiles/ranger/rc.conf ~/.config/ranger/rc.conf
