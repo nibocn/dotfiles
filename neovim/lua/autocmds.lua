ad4e3318a80d3429bf3aef0251d6ca84a78e2706
NiBo <nibocn@gmail.com>
2022-12-10 22:19:57 +0800

    config(neovim): 调整 autocmd 写法
    - 增加用 o 换行时不要延续注释回车换行可以；

--- neovim/lua/autocmds.lua
+++ neovim/lua/autocmds.lua
@@ -1,10 +1,21 @@
+local autocmd = vim.api.nvim_create_autocmd
+
+local foldFixGroup = vim.api.nvim_create_augroup('foldFixGroup', { clear = true, })
 -- 解决 telescope 插件导致代码折叠不生效的问题
-pcall(
-  vim.cmd,
-  [[
-    augroup fold_fix
-    autocmd!
-    autocmd BufRead * autocmd BufWinEnter * ++once normal! zx zR
-    augroup end
-  ]]
-)
+autocmd({'BufRead', 'BufWinEnter'}, {
+  group = foldFixGroup,
+  pattern = '*',
+  command = 'normal! zx zR',
+})
+
+local oNewlineGroup = vim.api.nvim_create_augroup('oNewlineGroup', { clear = true, })
+-- 用 o 换行不要延续注释
+autocmd("BufEnter", {
+  group = oNewlineGroup,
+  pattern = "*",
+  callback = function()
+    vim.opt.formatoptions = vim.opt.formatoptions
+      - "o" -- O and o, don't continue comments
+      + "r" -- But do continue when pressing enter.
+  end,
+})
