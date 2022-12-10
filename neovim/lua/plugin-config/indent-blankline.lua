9ae292ae64ae0e039f58a5523f87b62d2c950ccf
NiBo <nibocn@gmail.com>
2022-12-10 22:17:46 +0800

    config(neovim): Update indent-blankline config

--- neovim/lua/plugin-config/indent-blankline.lua
+++ neovim/lua/plugin-config/indent-blankline.lua
@@ -6,7 +6,7 @@ end
 
 indent_blankline.setup({
   -- 空行占位
-  -- space_char_blankline = " ",
+  space_char_blankline = " ",
   -- 用 treesitter 判断上下文
   show_current_context = true,
   show_current_context_start = true,
