d50aab34ff4883f1e26d0ac3b7e0f3cc5142f944
NiBo <nibocn@gmail.com>
2022-12-19 20:25:27 +0800

    config(neovim): 解决 python 虚拟环境不能正常识别依赖

--- neovim/lua/lsp/config/pyright.lua
+++ neovim/lua/lsp/config/pyright.lua
@@ -1,7 +1,29 @@
-local common_config = require("lsp.common-config")
+local common_config = require('lsp.common-config')
+local util = require('lspconfig.util')
+
+local function get_python_path(workspace)
+  -- Use activated virtualenv.
+  if vim.env.VIRTUAL_ENV then
+    return util.path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+  end
+
+  -- Find and use virtualenv from pipenv in workspace directory.
+  local match = vim.fn.glob(util.path.join(workspace, 'Pipfile'))
+  if match ~= '' then
+    local venv = vim.fn.trim(vim.fn.system('PIPENV_PIPFILE=' .. match .. ' pipenv --venv'))
+    return util.path.join(venv, 'bin', 'python')
+  end
+
+  -- Fallback to system Python.
+  return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
+end
+
 local opts = {
   capabilities = common_config.capabilities,
   flags = common_config.flags,
+  on_init = function(client)
+    client.config.settings.python.pythonPath = get_python_path(client.config.root_dir)
+  end,
   on_attach = function(client, bufnr)
     common_config.disableFormat(client)
     common_config.keyAttach(bufnr)
